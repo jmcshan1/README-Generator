cac3d48af735e07358b2d739be2a3b6ee24ceb4d
jmcshan1 <jmcshan01@gmail.com>
2023-08-05 11:45:01 -0500

    added function to write README file

--- index.js
+++ index.js
@@ -2,56 +2,63 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 const generateREADME = ({username, title,description, commandInstall, commandRun, usage, contributing, tests, email, license}) =>
-    `# ${title}
-    ![GitHub License]()
-    
-    ## Description
-    
-    ${description}
-    
-    ## Table of Contents
-    
-    *[Installation](#installation)
-    
-    *[Usage](#Usage)
-    
-    *[License](#License)
-    
-    *[Contributing](#Contributing)
-    
-    *[Tests](#Tests)
-    
-    *[Questions](#Questions)
-    
-    ## Installation
-    
-    ${installation}
-    
-    In order install the dependencies, run the following in the terminal:
-    
-    ${commandRun}
-    
-    ## Usage
-    
-    ${usage}
-    
-    ## License 
-    
-    ${license}
-    
-    ## Contributing
-    
-    ${contributing}
-    
-    ## Tests
-    
-    ${tests}
-    
-    ## Questions
-    
-    Email me at: ${email}`;
+`# ${title}
+
+## Description
+
+${description}
+
+## Table of Contents
+
+-[Installation](#installation)
+
+-[Usage](#Usage)
+
+-[License](#License)
+
+-[Contributing](#Contributing)
+
+-[Tests](#Tests)
+
+-[Questions](#Questions)
+
+## Installation
+
+In order install the dependencies, run the following in the terminal:
+
+${commandRun}
+
+## Usage
+
+${usage}
+
+## License 
+
+${license}
+
+## Contributing
+
+${contributing}
+
+## Tests
+
+${tests}
+
+## Questions
+
+Email me at: ${email}`;
 
 inquirer
     .prompt([
+        {
+            type: 
+        }
 
     ])
+    .then((data) => {
+        const readmeContent = generateREADME(data);
+
+        fs.writeFile('README.md', readmeContent, (err) => 
+        err ? console.log(err) : console.log('Success!')
+        );
+    })
